bccc4c473b7edb0b434923baed8aa8c5f6a23f90
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-02 23:14:59 +0530

    multiple company computation added

--- src/com/wage/empwage.java
+++ src/com/wage/empwage.java
@@ -2,34 +2,43 @@ package com.wage;
 
 public class empwage {
 
-	public static void main(String[] args) {
+	
 
-		System.out.println("Welcome to Employee wage Computation");
-		final int FULL_TIME = 1;
-		final int PART_TIME = 2;
-		int wageperHour = 20, fullDayHours = 0, totalWorkingHours = 0, totalWorkingDays = 0,empwage = 0, totalwage = 0;
-		while (totalWorkingHours < 100 || totalWorkingDays < 20) {
+		
+		public static final int FULL_TIME = 1;
+		public static final int PART_TIME = 2;
+		
+		public static int computeEmpwage (String company, int wageperhour, int totalworkingdays, int numofdays) {
+			
+		
+		int wageperHour = 0, fullDayHours = 0, totalWorkingHours = 0, totalWorkingDays = 0,empwage = 0;
+		while (totalWorkingHours <=fullDayHours && totalWorkingDays < numofdays) {
+			totalWorkingDays++;
 			int presence = (int) Math.floor(Math.random() * 10) % 3;
 			switch (presence) {
 			case FULL_TIME:
-				fullDayHours = 8;
-				totalWorkingDays += 1;
+				fullDayHours =8;
+				
 				break;
 			case PART_TIME:
 				fullDayHours = 4;
-				totalWorkingDays += 1;
+				
 				break;
 			default:
 				fullDayHours = 0;
 				break;
 			}
-			//toCalculate the employee wage, total wage and Total working hours
-			empwage = fullDayHours * wageperHour;
-			totalwage += empwage;
-			totalWorkingHours += fullDayHours;
-			System.out.println("Employee wage for a single day " + empwage);
-		}
-		System.out.println("Total employee wage " + totalwage);
-		System.out.println("Total workin hours " +totalWorkingHours);
+			totalWorkingHours+=fullDayHours;
+			System.out.println("Total workin hours " +totalWorkingHours);
+		}//toCalculate the employee wage, total wage and Total working hours
+			empwage = totalWorkingHours * wageperHour;
+
+		System.out.println("Total employee wage for the company "  +company +"is" + empwage);
+		return empwage;
+		
 	}
+public static void main(String[] args) {
+	computeEmpwage("supermart",20,2,10);
+	computeEmpwage("skmart",10,4,20);
+}
 }
